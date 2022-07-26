5b1258b555bd83f783865c2304ddaac0b042c3f5
Ahmet Burak TOKATLIOGLU <burak.tokatlioglu@bilgeadam.com>
2022-07-26 16:47:49 +0300

    final refactorings - last publish version indexed as v4

---
+++ IKBasvuru.DATA/Migrations/20220726075835_field-substraction.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IKBasvuru.DATA.Migrations
+{
+    public partial class fieldsubstraction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "JobApplication");
+
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "JobApplication");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "JobApplication");
+
+            migrationBuilder.DropColumn(
+                name: "MaritalStatus",
+                table: "JobApplication");
+
+            migrationBuilder.DropColumn(
+                name: "MilitaryService",
+                table: "JobApplication");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "JobApplication",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "JobApplication",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "JobApplication",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaritalStatus",
+                table: "JobApplication",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MilitaryService",
+                table: "JobApplication",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
